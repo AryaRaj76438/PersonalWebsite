e35b5d40b5c46b2293c6cd657d6f6ff1cc460962
Rajneesh Kumar <192125aryaraj@gmail.com>
2025-06-30 15:24:32 +0530

    Initial deply-ready commit

--- src/main/java/com/QuantumSyntax/blogsite/QuantumSyntaxApplication.java
+++ src/main/java/com/QuantumSyntax/blogsite/QuantumSyntaxApplication.java
@@ -2,6 +2,7 @@ package com.QuantumSyntax.blogsite;
 
 import com.QuantumSyntax.blogsite.model.Post;
 import com.QuantumSyntax.blogsite.repository.PostRepository;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +15,10 @@ public class QuantumSyntaxApplication {
 	private PostRepository postRepository;
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
 		SpringApplication.run(QuantumSyntaxApplication.class, args);
 	}
 
